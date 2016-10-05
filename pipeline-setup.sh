619066887c129a474cee69acb011307ddc75636d
Tariq Islam <tislam@redhat.com>
2016-10-05 17:19:20 -0400

    pipeline setup script, removed bc and is

---
+++ pipeline-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+oc login https://192.168.122.124:8443 --insecure-skip-tls-verify -u openshift-dev -p devel
+oc new-project ruby-mysql-app-dev --display-name="Application Development Environment"
+oc new-app --name=ruby-mysql-app https://github.com/tariq-islam/ruby-mysql-app#ocp33-pipeline
+oc new-app mysql-ephemeral
+oc new-app jenkins-ephemeral -p JENKINS_PASSWORD=password
+oc create -f https://raw.githubusercontent.com/tariq-islam/ruby-mysql-app/ocp33-pipeline/ruby-mysql-app-pipeline-bc.yml
+oc new-project ruby-mysql-app-qa --display-name="Application QA Environment"
+oc new-project ruby-mysql-app --display-name="Application Production Environment"
