7428b33810aee7395eb434c2dc8325e827de9969
Tariq Islam <tislam@redhat.com>
2016-10-07 19:41:19 -0400

    updated for easier setup

--- pipeline-setup.sh
+++ pipeline-setup.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+echo "Please enter your OpenShift hostname:port (https://<hostname>:<port>):"
 
-oc login https://192.168.122.124:8443 --insecure-skip-tls-verify -u openshift-dev -p devel
+read hostname
+
+echo "Enter your username: "
+
+read username
+
+oc login "$hostname" --insecure-skip-tls-verify -u "$username"
 oc new-project app-dev --display-name="Application Development Environment"
-oc new-app --name=ruby-mysql-app https://github.com/tariq-islam/ruby-mysql-app#ocp33-pipeline
+oc new-app --name=ruby-mysql https://github.com/tariq-islam/ruby-mysql-app#ocp33-pipeline
+oc expose service ruby-mysql
 oc new-app mysql-ephemeral
 oc new-app jenkins-ephemeral -p JENKINS_PASSWORD=password
-oc env dc/mysql --list | grep MYSQL | oc env dc/ruby-mysql-app -e -
+oc env dc/mysql --list | grep MYSQL | oc env dc/ruby-mysql -e -
 oc create -f https://raw.githubusercontent.com/tariq-islam/ruby-mysql-app/ocp33-pipeline/ruby-mysql-app-pipeline-bc.yml
 oc new-project app-qa --display-name="Application QA Environment"
 oc new-app mysql-ephemeral
 oc new-project app-prod --display-name="Application Production Environment"
 oc new-app mysql-ephemeral
+
